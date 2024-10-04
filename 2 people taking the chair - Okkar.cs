1f956962095d55d688a7e5f92633b23fc798fae8
Rioclaw11 <okkar.san@fwcsstudents.org>
2024-10-04 08:41:31 -0400

    Add files via upload

---
+++ 2 people taking the chair - Okkar.cs
@@ -0,0 +1,18 @@
+
+
+private void person_chair_Click(object taking , names)
+        {
+            Random rand = new Random();
+            List<string>person_chair = new List<string>();
+            person_chair.Add("Kyle");
+            person_chair.Add("Tenayia");
+            person_chair.Add("Gage");
+            person_chair.Add("Isaac");
+            person_chair.Add("Okkar");
+            person_chair.Add("Avonta");
+            for (int i =0; i < 6; i++)
+            {
+                int index = rand.Next(0, 6);
+                person_chair.Text = person_chair[index];
+            }
+        }
